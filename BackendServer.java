6a8233bded220750365f1c536c760ab6fe39b0d9
Karan Sharma <karanx11.72898@gmail.com>
2026-05-29 19:03:37 +0530

    Initial commit

---
+++ BackendServer.java
@@ -0,0 +1,34 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class BackendServer {
+
+    public static void main(String[] args) throws IOException {
+
+        int port = Integer.parseInt(args[0]);
+
+        HttpServer server =
+                HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/", exchange -> {
+
+            String response =
+                    "Response from Backend Server running on port "
+                            + port;
+
+            exchange.sendResponseHeaders(200, response.length());
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println(
+                "Backend Server Started on Port: " + port);
+    }
+}
